d83ab6fffa0a36e21d5f5a81bd081fd6a9c22521
Meryl Turner <merylturner08@gmail.com>
2017-07-10 14:30:12 -0700

    Created four passing tests, commented out divide 0 error

--- test/divide.test.js
+++ test/divide.test.js
@@ -3,7 +3,21 @@ const divide = require('../src/divide');
 
 describe('dividing two numbers', () => {
     it('divides two numbers', () => {
-        const quotient = divide(2,100);
+        const quotient = divide(2, 100);
         assert.equal(quotient, 50);
+
+        // let errored = false;
+        // try {
+        //     divide(0, 8);
+        // }
+        // catch (err) {
+        //     errored = true;
+        // }
+        // assert.ok(errored);
+
+        // if (errored) {
+        //     throw new Error('cannot divide by 0');
+        // }
     });
+
 });
