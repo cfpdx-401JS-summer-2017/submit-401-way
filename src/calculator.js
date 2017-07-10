bf8de4951214cb16557c33dca107de7f66033422
Christopher Thomson <christopher.thomson42@gmail.com>
2017-07-10 15:56:49 -0700

    Added other features

--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,17 @@
 
 function add(x,y) {
     return x + y;
-
 };
-module.exports = add;
+function subtract(x,y) {
+    return x - y;
+};
+function multiply(x,y) {
+    return x * y;
+};
+function devide(x,y) {
+    return x %= y;
+};
+module.exports = add; 
+module.exports =subtract;
+module.exports = multiply;
+module.exports =devide;
