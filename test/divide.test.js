719564220351a209e52432c00e13a97b076a9404
Meryl Turner <merylturner08@gmail.com>
2017-07-10 14:57:15 -0700

    removed commented code

--- test/divide.test.js
+++ test/divide.test.js
@@ -5,19 +5,6 @@ describe('dividing two numbers', () => {
     it('divides two numbers', () => {
         const quotient = divide(2, 100);
         assert.equal(quotient, 50);
-
-        // let errored = false;
-        // try {
-        //     divide(0, 8);
-        // }
-        // catch (err) {
-        //     errored = true;
-        // }
-        // assert.ok(errored);
-
-        // if (errored) {
-        //     throw new Error('cannot divide by 0');
-        // }
     });
 
 });
