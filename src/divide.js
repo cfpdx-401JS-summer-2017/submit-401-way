29edb241a4c59007cffab2df8084bc8ee6c5a767
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-07-10 17:11:58 -0700

    Correctly throw error when divide by zero

--- src/divide.js
+++ src/divide.js
@@ -1,5 +1,12 @@
 function divide(a,b) {
-    return a/b;
+    let result = 0;
+
+    if (b === 0) {
+        throw ('NaN--division by zero');
+    } else {
+        result = a / b;
+    }
+    return result;
 }
 
 module.exports = divide;
