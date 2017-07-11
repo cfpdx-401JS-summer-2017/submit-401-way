627257409ef17a3b829eb4541e740cf43aaa97d1
martypdx <marty@martypdx.com>
2017-07-10 19:46:51 -0700

    export object of functions example

---
+++ src/calculator.js
@@ -0,0 +1,24 @@
+function add(x, y) {
+    
+}
+
+function subtract(x, y) {
+
+}
+
+function multiple(x, y) {
+
+}
+
+function divide(x, y) {
+
+}
+
+module.exports = {
+    add,
+    subtract,
+    multiple,
+    divide
+};
+
+
---
+++ tests/calculator.test.js
@@ -0,0 +1,6 @@
+const calculator = require('../src/calculator');
+
+// calculator.add()
+// calculator.subtract()
+// calculator.multiple()
+// calculator.divide()
