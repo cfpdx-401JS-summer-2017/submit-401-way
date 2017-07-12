aa6f5dd235d5cd07bee977371a6539064907ce88
Niilo Smeds <niilosmeds@gmail.com>
2017-07-12 10:09:51 -0700

    Revert "Added add function and related mocha test"

--- tests/subtract.test.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-var subtract = require('../src/subtract');
-
-describe('subtracting two numbers', () => {
-    it('subtracts two numbers', () => {
-        const test_difference = subtract(1, 2);
-        assert.equal(test_difference, -1);
-    });
-});
